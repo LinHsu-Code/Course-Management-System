8aefa584247fda1adfe13847029b9af3c237fc2f
Lin <dbllinxu@gmail.com>
2022-04-20 13:04:20 +0800

    feat: getStudentList api request

--- pages/dashboard/manager/students/index.tsx
+++ pages/dashboard/manager/students/index.tsx
@@ -1,96 +1,98 @@
 import Head from 'next/head'
 import type { ReactElement } from 'react'
+import { useState, useEffect } from 'react'
 import { Table, Tag, Radio, Space } from 'antd'
 import DashboardLayout from '../../../../components/dashboardLayout'
-
-const topOptions = [
-  { label: 'topLeft', value: 'topLeft' },
-  { label: 'topCenter', value: 'topCenter' },
-  { label: 'topRight', value: 'topRight' },
-  { label: 'none', value: 'none' },
-]
-
-const bottomOptions = [
-  { label: 'bottomLeft', value: 'bottomLeft' },
-  { label: 'bottomCenter', value: 'bottomCenter' },
-  { label: 'bottomRight', value: 'bottomRight' },
-  { label: 'none', value: 'none' },
-]
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    render: (text) => <a>{text}</a>,
-  },
-  {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age',
-  },
-  {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address',
-  },
-  {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (tags) => (
-      <span>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green'
-          if (tag === 'loser') {
-            color = 'volcano'
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          )
-        })}
-      </span>
-    ),
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-      <Space size="middle">
-        <a>Invite {record.name}</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-]
-
-const data = [
-  {
-    key: '1',
-    name: 'John Brown',
-    age: 32,
-    address: 'New York No. 1 Lake Park',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'Jim Green',
-    age: 42,
-    address: 'London No. 1 Lake Park',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'Joe Black',
-    age: 32,
-    address: 'Sidney No. 1 Lake Park',
-    tags: ['cool', 'teacher'],
-  },
-]
+import { getStudentList } from '../../../../lib/httpRequest'
 
 export default function Dashboard() {
+  const [page, setPage] = useState(1)
+  const [limit, setLimit] = useState(20)
+
+  useEffect(() => {
+    getStudentList({ page, limit }).then((res) => {
+      if (res.data) {
+        console.log(res.data)
+      }
+    })
+  }, [])
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: (tags) => (
+        <span>
+          {tags.map((tag) => {
+            let color = tag.length > 5 ? 'geekblue' : 'green'
+            if (tag === 'loser') {
+              color = 'volcano'
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            )
+          })}
+        </span>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ]
+
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ]
+
+  const changePagination = (page, pageSize) => {
+    console.log(page, pageSize)
+  }
   return (
     <>
       <Head>
@@ -99,7 +101,7 @@ export default function Dashboard() {
       <div>
         <Table
           columns={columns}
-          pagination={{ defaultPageSize: 20 }}
+          pagination={{ defaultPageSize: 20, onChange: changePagination }}
           dataSource={data}
         />
       </div>
