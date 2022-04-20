01ab9124a9c5d1ec6e3fa40fb5bd5bf647074020
Lin <dbllinxu@gmail.com>
2022-04-20 11:54:58 +0800

    feat: students list antd component

---
+++ pages/dashboard/manager/students/index.tsx
@@ -0,0 +1,112 @@
+import Head from 'next/head'
+import type { ReactElement } from 'react'
+import { Table, Tag, Radio, Space } from 'antd'
+import DashboardLayout from '../../../../components/dashboardLayout'
+
+const topOptions = [
+  { label: 'topLeft', value: 'topLeft' },
+  { label: 'topCenter', value: 'topCenter' },
+  { label: 'topRight', value: 'topRight' },
+  { label: 'none', value: 'none' },
+]
+
+const bottomOptions = [
+  { label: 'bottomLeft', value: 'bottomLeft' },
+  { label: 'bottomCenter', value: 'bottomCenter' },
+  { label: 'bottomRight', value: 'bottomRight' },
+  { label: 'none', value: 'none' },
+]
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+  {
+    title: 'Tags',
+    key: 'tags',
+    dataIndex: 'tags',
+    render: (tags) => (
+      <span>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? 'geekblue' : 'green'
+          if (tag === 'loser') {
+            color = 'volcano'
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          )
+        })}
+      </span>
+    ),
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+]
+
+const data = [
+  {
+    key: '1',
+    name: 'John Brown',
+    age: 32,
+    address: 'New York No. 1 Lake Park',
+    tags: ['nice', 'developer'],
+  },
+  {
+    key: '2',
+    name: 'Jim Green',
+    age: 42,
+    address: 'London No. 1 Lake Park',
+    tags: ['loser'],
+  },
+  {
+    key: '3',
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+    tags: ['cool', 'teacher'],
+  },
+]
+
+export default function Dashboard() {
+  return (
+    <>
+      <Head>
+        <title>{'CMS DashBoard: Manager'}</title>
+      </Head>
+      <div>
+        <Table
+          columns={columns}
+          pagination={{ defaultPageSize: 20 }}
+          dataSource={data}
+        />
+      </div>
+    </>
+  )
+}
+
+Dashboard.getLayout = function getLayout(data: any, component: ReactElement) {
+  return <DashboardLayout>{component}</DashboardLayout>
+}
