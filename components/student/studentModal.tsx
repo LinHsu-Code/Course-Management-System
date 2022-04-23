e0b4dcd44c3d6274b2a518a60b17abee899e43f2
Lin <dbllinxu@gmail.com>
2022-04-23 20:53:59 +0800

    feat: add student function done

--- components/student/studentModal.tsx
+++ components/student/studentModal.tsx
@@ -1,103 +1,108 @@
 import { Modal, Select, Form, Input } from 'antd'
-import { time } from 'console'
-import { CountryList } from '../../lib/contants'
+import { CountryList } from '../../lib/constants'
+import { AddStudent } from '../../lib/model'
+import { addStudent } from '../../lib/httpRequest'
 
 export default function StudentModal(props: any) {
-  const onFinish = (values) => {
-    console.log('Success:', values)
+  const [form] = Form.useForm()
+  const handleOk = async () => {
+    const formData: AddStudent = form.getFieldsValue(true)
+    const res = await addStudent(formData)
+    if (res.data) {
+      form.resetFields()
+      props.setIsModalVisible(false)
+    }
   }
 
-  const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo)
+  const handleCancel = () => {
+    form.resetFields()
+    props.setIsModalVisible(false)
   }
   return (
-    <>
-      <Modal
-        title="Add Student"
-        cancelText="Cancel"
-        okText="Add"
-        destroyOnClose
-        centered
-        visible={props.isModalVisible}
-        onOk={() => props.setIsModalVisible(false)}
-        onCancel={() => props.setIsModalVisible(false)}
-      >
-        <Form
-          name="student_modal"
-          labelCol={{
-            span: 8,
-          }}
-          wrapperCol={{
-            span: 16,
-          }}
-          initialValues={
-            {
-              // remember: true,
-            }
+    <Modal
+      title="Add Student"
+      cancelText="Cancel"
+      okText="Add"
+      destroyOnClose={true}
+      centered={true}
+      visible={props.isModalVisible}
+      onOk={() => handleOk()}
+      onCancel={() => handleCancel()}
+    >
+      <Form
+        name="student_modal"
+        form={form}
+        labelCol={{
+          span: 6,
+        }}
+        wrapperCol={{
+          span: 18,
+        }}
+        initialValues={
+          {
+            // remember: true,
           }
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
+        }
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Name"
+          name="name"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
         >
-          <Form.Item
-            label="Name"
-            name="name"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
+          <Input />
+        </Form.Item>
 
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              {
-                required: true,
-                type: 'email',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              type: 'email',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-          <Form.Item
-            label="Area"
-            name="country"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Select>
-              {CountryList.map((item, index) => (
-                <Select.Option value={item} key={index}>
-                  {item}
-                </Select.Option>
-              ))}
-            </Select>
-          </Form.Item>
+        <Form.Item
+          label="Area"
+          name="country"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Select>
+            {CountryList.map((item, index) => (
+              <Select.Option value={item} key={index}>
+                {item}
+              </Select.Option>
+            ))}
+          </Select>
+        </Form.Item>
 
-          <Form.Item
-            label="Student Type"
-            name="type"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-          >
-            <Select>
-              <Select.Option value={1}>Tester</Select.Option>
-              <Select.Option value={2}>Developer</Select.Option>
-            </Select>
-          </Form.Item>
-        </Form>
-      </Modal>
-    </>
+        <Form.Item
+          label="Student Type"
+          name="type"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Select>
+            <Select.Option value={1}>Tester</Select.Option>
+            <Select.Option value={2}>Developer</Select.Option>
+          </Select>
+        </Form.Item>
+      </Form>
+    </Modal>
   )
 }
