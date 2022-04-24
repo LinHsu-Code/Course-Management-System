a641e96753a776c43b4259116c5bcd9216d99c28
Lin <dbllinxu@gmail.com>
2022-04-24 23:27:42 +0800

    fix: Instance created by useForm is not connect to any Form element

--- components/student/studentModal.tsx
+++ components/student/studentModal.tsx
@@ -25,86 +25,89 @@ export default function StudentModal(props: any) {
     props.setIsModalVisible(false)
   }
 
+  const modalForm = (
+    <Form
+      name="student_modal"
+      form={form}
+      labelCol={{
+        span: 6,
+      }}
+      wrapperCol={{
+        span: 18,
+      }}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Name"
+        name="name"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            type: 'email',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Area"
+        name="country"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Select>
+          {COUNTRY_LIST.map((item, index) => (
+            <Select.Option value={item} key={index}>
+              {item}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+
+      <Form.Item
+        label="Student Type"
+        name="type"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Select>
+          <Select.Option value={1}>Tester</Select.Option>
+          <Select.Option value={2}>Developer</Select.Option>
+        </Select>
+      </Form.Item>
+    </Form>
+  )
   return (
     <Modal
       title={props.isEdit ? 'Edit Student' : 'Add Student'}
       cancelText="Cancel"
       okText={props.isEdit ? 'Update' : 'Add'}
-      destroyOnClose
+      destroyOnClose={true}
       centered={true}
       visible={props.isModalVisible}
       onOk={() => handleOk()}
       onCancel={() => handleClose()}
     >
-      <Form
-        name="student_modal"
-        form={form}
-        labelCol={{
-          span: 6,
-        }}
-        wrapperCol={{
-          span: 18,
-        }}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Name"
-          name="name"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[
-            {
-              required: true,
-              type: 'email',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Area"
-          name="country"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Select>
-            {COUNTRY_LIST.map((item, index) => (
-              <Select.Option value={item} key={index}>
-                {item}
-              </Select.Option>
-            ))}
-          </Select>
-        </Form.Item>
-
-        <Form.Item
-          label="Student Type"
-          name="type"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Select>
-            <Select.Option value={1}>Tester</Select.Option>
-            <Select.Option value={2}>Developer</Select.Option>
-          </Select>
-        </Form.Item>
-      </Form>
+      {modalForm}
     </Modal>
   )
 }
