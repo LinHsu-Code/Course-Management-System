026b26cc80ec028b7b5fbfcba60fb23baace4465
Lin <dbllinxu@gmail.com>
2022-05-21 17:24:29 +0800

    feat: add course steps

--- pages/dashboard/manager/courses/add-course/index.tsx
+++ pages/dashboard/manager/courses/add-course/index.tsx
@@ -1,12 +1,34 @@
 import Head from 'next/head'
+import { Steps } from 'antd'
+import { useState } from 'react'
 
+const { Step } = Steps
+
+function getDisabled(stepNumber: number, finishedSteps: number) {
+  return stepNumber > finishedSteps ? true : false
+}
 export default function Page() {
+  const [current, setCurrent] = useState(0)
+  const [finishedSteps, setFinishedSteps] = useState(0)
+
   return (
     <>
       <Head>
         <title>{'CMS DashBoard: Manager-Add Course'}</title>
       </Head>
-      <div>Manager-Add Course</div>
+      <Steps
+        type="navigation"
+        current={current}
+        onChange={(current) => setCurrent(current)}
+        className="site-navigation-steps"
+      >
+        <Step title="Course Detail" />
+        <Step
+          disabled={getDisabled(1, finishedSteps)}
+          title="Course Schedule"
+        />
+        <Step disabled={getDisabled(2, finishedSteps)} title="Success" />
+      </Steps>
     </>
   )
 }
