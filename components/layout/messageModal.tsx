89e84d987c55b8c3bfd4fb84b717e86dfc42d9b3
Lin <dbllinxu@gmail.com>
2022-06-02 16:23:46 +0800

    feat: custom messageModal

--- components/layout/messageModal.tsx
+++ components/layout/messageModal.tsx
@@ -1,6 +1,19 @@
-import { Layout, Row, Col, Modal, Button, Tabs } from 'antd'
+import { Row, Col, Modal, Button, Tabs } from 'antd'
+import styled from 'styled-components'
 
 const { TabPane } = Tabs
+
+const CustomModal = styled(Modal)`
+  .ant-modal-content {
+    border-radius: 4px;
+  }
+  .ant-modal-body {
+    padding: 12px 24px;
+  }
+  .ant-modal-footer {
+    padding: 0;
+  }
+`
 
 export default function MessageModal({
   modal1Visible,
@@ -10,7 +23,7 @@ export default function MessageModal({
   setModal1Visible: (modal1Visible: boolean) => void
 }) {
   return (
-    <Modal
+    <CustomModal
       closable={false}
       style={{ top: 68, marginRight: 56, marginLeft: 'auto' }}
       visible={modal1Visible}
@@ -19,7 +32,10 @@ export default function MessageModal({
       width={400}
       footer={[
         <Row key="footer" style={{ textAlign: 'center' }}>
-          <Col span={12} style={{ borderRight: '1px solid #f0f0f0' }}>
+          <Col
+            span={12}
+            style={{ borderRight: '1px solid #f0f0f0', padding: 10 }}
+          >
             <Button type="text">Mark all as read</Button>
           </Col>
           <Col span={12}>
@@ -36,6 +52,6 @@ export default function MessageModal({
           Content of Tab Pane 2
         </TabPane>
       </Tabs>
-    </Modal>
+    </CustomModal>
   )
 }
